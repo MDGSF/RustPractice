86c3534cc7f3844125ec9b368f331bbe23497bb8
huangjian <1342042894@qq.com>
2019-01-05 16:25:38 +0800

    add variables

---
+++ variables/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    const MAX_POINTS: u32 = 100_000;
+    println!("max points = {}", MAX_POINTS);
+
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+}
