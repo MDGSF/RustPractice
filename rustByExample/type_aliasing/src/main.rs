43c39daceee192518d8a0ef0a5c4e9a2cce79858
huangjian <1342042894@qq.com>
2019-01-16 16:21:19 +0800

    demo

---
+++ rustByExample/type_aliasing/src/main.rs
@@ -0,0 +1,19 @@
+type NanoSecond = u64;
+type Inch = u64;
+
+#[allow(non_camel_case_types)]
+type u64_t = u64;
+
+fn main() {
+    println!("Hello, world!");
+
+    let nanoseconds: NanoSecond = 5 as u64_t;
+    let inches: Inch = 2 as u64_t;
+
+    println!(
+        "{} nanoseconds + {} inches = {} unit?",
+        nanoseconds,
+        inches,
+        nanoseconds + inches
+    );
+}
---
+++ rustByExample/type_inference/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+
+    let elem = 5u8;
+
+    let mut vec = Vec::new();
+
+    vec.push(elem);
+
+    println!("{:?}", vec);
+}
