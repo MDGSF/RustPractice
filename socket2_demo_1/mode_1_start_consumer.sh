0d9f86d26da59867554c8f34415dcf6aa482faab
HuangJian <1342042894@qq.com>
2020-09-25 14:45:51 +0800

    add demos

---
+++ socket2_demo_1/mode_1_start_consumer.sh
@@ -0,0 +1,11 @@
+./consumer 0.0.0.0:1600 224.168.2.10 > mode_1_log_0.txt &
+./consumer 0.0.0.0:1601 224.168.2.11 > mode_1_log_1.txt &
+./consumer 0.0.0.0:1602 224.168.2.12 > mode_1_log_2.txt &
+./consumer 0.0.0.0:1603 224.168.2.13 > mode_1_log_3.txt &
+./consumer 0.0.0.0:1604 224.168.2.14 > mode_1_log_4.txt &
+./consumer 0.0.0.0:1605 224.168.2.15 > mode_1_log_5.txt &
+./consumer 0.0.0.0:1606 224.168.2.16 > mode_1_log_6.txt &
+./consumer 0.0.0.0:1607 224.168.2.17 > mode_1_log_7.txt &
+./consumer 0.0.0.0:1608 224.168.2.18 > mode_1_log_8.txt &
+./consumer 0.0.0.0:1609 224.168.2.19 > mode_1_log_9.txt &
+
