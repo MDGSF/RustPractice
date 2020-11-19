4840df311376a615a3becf792df9d66e762be5a5
HuangJian <1342042894@qq.com>
2020-11-19 20:18:58 +0800

    feat: update exercism

--- exercism/parallel-letter-frequency/src/lib.rs
+++ exercism/parallel-letter-frequency/src/lib.rs
@@ -1,25 +1,44 @@
 use std::collections::HashMap;
+use std::thread;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-  (0..worker_count)
-    .map(|i| {
-      let mut result = HashMap::new();
-      input
-        .join("")
-        .chars()
-        .skip(i)
-        .step_by(worker_count)
-        .filter(|c| c.is_alphabetic())
-        .map(|c| c.to_ascii_lowercase())
-        .for_each(|c| {
-          *result.entry(c).or_insert(0) += 1;
-        });
-      result
-    })
-    .fold(HashMap::new(), |mut result, m| {
-      m.iter().for_each(|(&k, &v)| {
-        *result.entry(k).or_insert(0) += v;
-      });
-      result
-    })
+  let input = input.join("");
+  if input.len() == 0 {
+    return HashMap::new();
+  }
+
+  let str_frequency = |input: &str| -> HashMap<char, usize> {
+    let mut result: HashMap<char, usize> = HashMap::new();
+    for ch in input
+      .chars()
+      .filter(|ch| ch.is_alphabetic())
+      .map(|ch| ch.to_ascii_lowercase())
+    {
+      *result.entry(ch).or_default() += 1;
+    }
+    result
+  };
+
+  let mut churn = input.chars();
+  let real_worker_count = worker_count.min(input.len());
+  let mut thread_pool = Vec::with_capacity(real_worker_count);
+  let work_length = (input.len() / real_worker_count).max(1);
+
+  (1..=real_worker_count).for_each(|i| {
+    let chunk = if i == real_worker_count {
+      churn.by_ref().collect::<String>()
+    } else {
+      churn.by_ref().take(work_length).collect::<String>()
+    };
+    thread_pool.push(thread::spawn(move || str_frequency(&chunk)));
+  });
+
+  let mut result: HashMap<char, usize> = HashMap::new();
+  for my_thread in thread_pool {
+    let sub = my_thread.join().unwrap();
+    for (&ch, num) in sub.iter() {
+      *result.entry(ch).or_default() += num;
+    }
+  }
+  result
 }
--- testdemo/src/main.rs
+++ testdemo/src/main.rs
@@ -1,4 +1,49 @@
+use std::collections::HashMap;
+use std::thread;
+
+pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
+  let input = input.join("");
+  if input.len() == 0 {
+    return HashMap::new();
+  }
+
+  let str_frequency = |input: &str| -> HashMap<char, usize> {
+    let mut result: HashMap<char, usize> = HashMap::new();
+    for ch in input
+      .chars()
+      .filter(|ch| ch.is_alphabetic())
+      .map(|ch| ch.to_ascii_lowercase())
+    {
+      *result.entry(ch).or_default() += 1;
+    }
+    result
+  };
+
+  let mut churn = input.chars();
+  let real_worker_count = worker_count.min(input.len());
+  let mut thread_pool = Vec::with_capacity(real_worker_count);
+  let work_length = (input.len() / real_worker_count).max(1);
+
+  (1..=real_worker_count).for_each(|i| {
+    let chunk = if i == real_worker_count {
+      churn.by_ref().collect::<String>()
+    } else {
+      churn.by_ref().take(work_length).collect::<String>()
+    };
+    thread_pool.push(thread::spawn(move || str_frequency(&chunk)));
+  });
+
+  let mut result: HashMap<char, usize> = HashMap::new();
+  for my_thread in thread_pool {
+    let sub = my_thread.join().unwrap();
+    for (&ch, num) in sub.iter() {
+      *result.entry(ch).or_default() += num;
+    }
+  }
+  result
+}
+
 fn main() {
-  let a = stringify!(1 + 1);
-  println!("{}", a);
+  let result = frequency(&["asdfg"], 2);
+  println!("{:?}", result);
 }
