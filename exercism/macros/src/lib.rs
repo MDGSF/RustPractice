eebd41b9ca83563b979ec15eba64d474140a7dca
HuangJian <1342042894@qq.com>
2020-11-29 14:44:45 +0800

    add exercism

--- exercism/macros/src/lib.rs
+++ exercism/macros/src/lib.rs
@@ -1,11 +1,8 @@
 #[macro_export(local_inner_macros)]
 macro_rules! hashmap {
-  ($($key:expr => $val:expr,)+) => { hashmap!($($key => $val),+) };
-  ($($key:expr => $val:expr),*) => {{
-    let mut hm = ::std::collections::HashMap::new();
-    $(
-      hm.insert($key, $val);
-    )*
-    hm
-  }};
+  ($($( $key:expr => $val:expr )+$(,)?)*) => {{
+    let mut map = ::std::collections::HashMap::new();
+    $($( map.insert($key, $val); )*)*
+    map
+  }}
 }
