acd39cae43b838f013edb4248b567f36134bc9fa
HuangJian <1342042894@qq.com>
2020-12-14 15:31:40 +0800

    add tokio echo server

---
+++ async_demos/tokio_echo_server/src/main.rs
@@ -0,0 +1,29 @@
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+  let listener = TcpListener::bind("127.0.0.1:6142").await.unwrap();
+
+  loop {
+    let (mut socket, _) = listener.accept().await?;
+
+    tokio::spawn(async move {
+      let mut buf = vec![0; 1024];
+
+      loop {
+        match socket.read(&mut buf).await {
+          Ok(0) => return,
+          Ok(n) => {
+            if socket.write_all(&buf[..n]).await.is_err() {
+              return;
+            }
+          }
+          Err(_) => {
+            return;
+          }
+        }
+      }
+    });
+  }
+}
