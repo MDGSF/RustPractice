e95a34ed5939995c2c9e6390e97e7704976ab02a
HuangJian <1342042894@qq.com>
2020-12-17 12:08:48 +0800

    feat: update s0 account manager

--- s0_account_manager/views/static/s0_account_manager/s0_account_manager.js
+++ s0_account_manager/views/static/s0_account_manager/s0_account_manager.js
@@ -1,10 +1,17 @@
-window.onload = function exampleFunction() {
+// window.onload = function exampleFunction() {
+//   console.log("window.onload");
+//   get_all_c1_account();
+// };
+
+$(document).ready(function () {
+  console.log("ready!");
   get_all_c1_account();
-};
+});
 
 function get_all_c1_account() {
   $.get("/get_all_c1_account", function (data) {
     console.log(data);
+    $("#list_s0_accounts").empty();
     for (let i = 0; i < data.accounts.length; i++) {
       $("#list_s0_accounts").append(
         `<li class="list-group-item">[${i + 1}]: ${data.accounts[i]}</li>`
@@ -16,6 +23,18 @@ function get_all_c1_account() {
 $("#btn_add_user").click(function () {
   let name = $("#input_add_user_name").val();
   let key = $("#input_add_user_key").val();
+  if (
+    name === null ||
+    key === null ||
+    name === undefined ||
+    key === undefined ||
+    name.length === 0 ||
+    key.length === 0
+  ) {
+    alert("invalid name or key");
+    return;
+  }
+
   $.ajax({
     type: "POST",
     url: "/add_c1_account",
@@ -23,12 +42,18 @@ $("#btn_add_user").click(function () {
     data: JSON.stringify({ name: name, key: key }),
     success: function (data) {
       console.log(data);
+      get_all_c1_account();
     },
   });
 });
 
 $("#btn_del_user").click(function () {
   let name = $("#input_del_user_name").val();
+  if (name === null || name === undefined || name.length === 0) {
+    alert("invalid name");
+    return;
+  }
+
   $.ajax({
     type: "POST",
     url: "/del_c1_account",
@@ -36,6 +61,7 @@ $("#btn_del_user").click(function () {
     data: JSON.stringify({ name: name }),
     success: function (data) {
       console.log(data);
+      get_all_c1_account();
     },
   });
 });
