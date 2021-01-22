f698a81385820c83c78afd98fe8f8e1892d4b4ee
HuangJian <1342042894@qq.com>
2021-01-22 19:13:23 +0800

    update awak study

---
+++ StudyAsyncAwait/awak/src/blocking.rs
@@ -0,0 +1,99 @@
+use std::cell::Cell;
+use std::future::Future;
+use std::sync::{
+  atomic::{AtomicBool, Ordering},
+  Arc,
+};
+use std::task::{Context, Poll};
+use std::time::{Duration, Instant};
+
+use crate::io::Reactor;
+use crate::parking;
+use crate::waker_fn::waker_fn;
+
+struct CallOnDrop<F: Fn()>(F);
+
+impl<F: Fn()> Drop for CallOnDrop<F> {
+  fn drop(&mut self) {
+    (self.0)();
+  }
+}
+
+pub fn block_on<T>(future: impl Future<Output = T>) -> T {
+  Reactor::get().block_on_count.fetch_add(1, Ordering::SeqCst);
+
+  let _guard = CallOnDrop(|| {
+    Reactor::get().block_on_count.fetch_sub(1, Ordering::SeqCst);
+    Reactor::get().unparker.unpark();
+  });
+
+  let (p, u) = parking::pair();
+
+  let io_blocked = Arc::new(AtomicBool::new(false));
+
+  thread_local! {
+    static IO_POLLING: Cell<bool> = Cell::new(false);
+  }
+
+  let waker = waker_fn({
+    let io_blocked = io_blocked.clone();
+    move || {
+      if u.unpark() && !IO_POLLING.with(Cell::get) && io_blocked.load(Ordering::SeqCst) {
+        Reactor::get().notify();
+      }
+    }
+  });
+
+  let cx = &mut Context::from_waker(&waker);
+
+  pin_mut!(future);
+
+  loop {
+    if let Poll::Ready(t) = future.as_mut().poll(cx) {
+      return t;
+    }
+
+    if p.park_timeout(Some(Duration::from_secs(0))) {
+      if let Some(mut reactor_lock) = Reactor::get().try_lock() {
+        IO_POLLING.with(|io| io.set(true));
+        let _guard = CallOnDrop(|| {
+          IO_POLLING.with(|io| io.set(false));
+        });
+        let _ = reactor_lock.react(Some(Duration::from_secs(0)));
+      }
+      continue;
+    }
+
+    if let Some(mut reactor_lock) = Reactor::get().try_lock() {
+      let start = Instant::now();
+
+      loop {
+        IO_POLLING.with(|io| io.set(true));
+        io_blocked.store(true, Ordering::SeqCst);
+        let _guard = CallOnDrop(|| {
+          IO_POLLING.with(|io| io.set(false));
+          io_blocked.store(false, Ordering::SeqCst);
+        });
+
+        if p.park_timeout(Some(Duration::from_secs(0))) {
+          break;
+        }
+
+        let _ = reactor_lock.react(None);
+
+        if p.park_timeout(Some(Duration::from_secs(0))) {
+          break;
+        }
+
+        if start.elapsed() > Duration::from_micros(500) {
+          drop(reactor_lock);
+          Reactor::get().unparker.unpark();
+          p.park();
+          break;
+        }
+      }
+    } else {
+      p.park();
+    }
+  }
+}
--- StudyAsyncAwait/awak/src/lib.rs
+++ StudyAsyncAwait/awak/src/lib.rs
@@ -19,6 +19,7 @@ macro_rules! ready {
   };
 }
 
+pub mod blocking;
 pub mod executor;
 pub mod io;
 pub mod net;
@@ -26,4 +27,28 @@ mod parking;
 pub mod time;
 mod waker_fn;
 
+use std::future::Future;
+use std::thread;
+
+pub use async_task::Task;
+pub use blocking::block_on;
 pub use executor::Executor;
+
+pub use futures_util::stream::StreamExt;
+
+use once_cell::sync::Lazy;
+
+pub static EXECUTOR: Lazy<Executor> = Lazy::new(||{
+  for _ in 0..num_cpus::get().max(1) {
+    thread::spawn(move || {
+      let ticker = EXECUTOR.ticker();
+      block_on(ticker.run());
+    });
+  }
+
+  Executor::new()
+});
+
+pub fn spawn<T: Send + 'static>(future: impl Future<Output = T> + Send + 'static) -> Task<T> {
+  EXECUTOR.spawn(future)
+}
