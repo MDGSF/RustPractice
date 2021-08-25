eddcf89c0df2eb9552ba570e91dffb71e175af02
HuangJian <1342042894@qq.com>
2021-08-25 16:15:01 +0800

    add poem hello world

---
+++ StudyWebFramework/poem_demos/hello_world/src/main.rs
@@ -0,0 +1,13 @@
+use poem::{handler, route, web::Path, Server};
+
+#[handler(method = "get")]
+fn hello(Path(name): Path<String>) -> String {
+  format!("hello, {}", name)
+}
+
+#[tokio::main]
+async fn main() {
+  let app = route().at("/hello/:name", hello);
+  let server = Server::bind("127.0.0.1:3000").await.unwrap();
+  server.run(app).await.unwrap();
+}
