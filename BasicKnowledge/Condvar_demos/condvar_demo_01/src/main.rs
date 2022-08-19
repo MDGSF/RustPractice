d624ae649fa80355009645b16d3e7e74c029e66c
HuangJian <1342042894@qq.com>
2022-08-19 19:54:43 +0800

    add condvar

---
+++ BasicKnowledge/Condvar_demos/condvar_demo_01/src/main.rs
@@ -0,0 +1,20 @@
+use std::sync::{Arc, Condvar, Mutex};
+use std::thread;
+
+fn main() {
+    let pair = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair2 = Arc::clone(&pair);
+
+    thread::spawn(move || {
+        let (lock, cvar) = &*pair2;
+        let mut started = lock.lock().unwrap();
+        *started = true;
+        cvar.notify_one();
+    });
+
+    let (lock, cvar) = &*pair;
+    let mut started = lock.lock().unwrap();
+    while !*started {
+        started = cvar.wait(started).unwrap();
+    }
+}
