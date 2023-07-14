8f76493cb75446361b4d33d742e9bb2d71db4ab7
HuangJian <huangjian@minieye.cc>
2023-07-14 16:56:12 +0800

    add rust atomics and locks chapter 4

---
+++ StudyRustAtomicAndLocks/chapter_4/s3_guard/src/main.rs
@@ -0,0 +1,16 @@
+use s3_guard::spin_lock::*;
+use std::thread;
+
+fn main() {
+    let x = SpinLock::new(Vec::new());
+    thread::scope(|s| {
+        s.spawn(|| x.lock().push(1));
+        s.spawn(|| {
+            let mut g = x.lock();
+            g.push(2);
+            g.push(2);
+        });
+    });
+    let g = x.lock();
+    assert!(g.as_slice() == [1, 2, 2] || g.as_slice() == [2, 2, 1]);
+}
