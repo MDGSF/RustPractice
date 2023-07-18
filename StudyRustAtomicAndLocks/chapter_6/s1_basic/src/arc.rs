b688ca6ed34892be8ae9cd2d0a3f1c0062a404e1
HuangJian <huangjian@minieye.cc>
2023-07-18 11:49:48 +0800

    add chapter 6 s1 basic demos

---
+++ StudyRustAtomicAndLocks/chapter_6/s1_basic/src/arc.rs
@@ -0,0 +1,69 @@
+use std::ops::Deref;
+use std::ptr::NonNull;
+use std::sync::atomic::fence;
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
+
+struct ArcData<T> {
+    ref_count: AtomicUsize,
+    data: T,
+}
+
+pub struct Arc<T> {
+    ptr: NonNull<ArcData<T>>,
+}
+
+unsafe impl<T: Send + Sync> Send for Arc<T> {}
+unsafe impl<T: Send + Sync> Sync for Arc<T> {}
+
+impl<T> Arc<T> {
+    pub fn new(data: T) -> Arc<T> {
+        Arc {
+            ptr: NonNull::from(Box::leak(Box::new(ArcData {
+                ref_count: AtomicUsize::new(1),
+                data,
+            }))),
+        }
+    }
+
+    fn data(&self) -> &ArcData<T> {
+        unsafe { self.ptr.as_ref() }
+    }
+
+    pub fn get_mut(arc: &mut Self) -> Option<&mut T> {
+        if arc.data().ref_count.load(Relaxed) == 1 {
+            fence(Acquire);
+            unsafe { Some(&mut arc.ptr.as_mut().data) }
+        } else {
+            None
+        }
+    }
+}
+
+impl<T> Deref for Arc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.data().data
+    }
+}
+
+impl<T> Clone for Arc<T> {
+    fn clone(&self) -> Self {
+        if self.data().ref_count.fetch_add(1, Relaxed) > usize::MAX / 2 {
+            std::process::abort();
+        }
+        Arc { ptr: self.ptr }
+    }
+}
+
+impl<T> Drop for Arc<T> {
+    fn drop(&mut self) {
+        if self.data().ref_count.fetch_sub(1, Release) == 1 {
+            fence(Acquire);
+            unsafe {
+                drop(Box::from_raw(self.ptr.as_ptr()));
+            }
+        }
+    }
+}
