394f7e6ffd518f2841a339fafbedd42d5c9e2a5f
huangjian <huangjian@bicv.com>
2024-09-11 18:32:55 +0800

    feat: update nom

---
+++ StudyCrates/nom_demos/nom_json_01/src/json_parser.rs
@@ -0,0 +1,36 @@
+use nom::combinator::map;
+use nom::{branch::alt, bytes::complete::tag, IResult};
+
+#[derive(PartialEq, Debug)]
+pub enum JsonBool {
+    False,
+    True,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct JsonNull {}
+
+fn json_bool(input: &str) -> IResult<&str, JsonBool> {
+    let parser = alt((tag("false"), tag("true")));
+    map(parser, |s| match s {
+        "false" => JsonBool::False,
+        "true" => JsonBool::True,
+        _ => unreachable!(),
+    })(input)
+}
+
+fn json_null(input: &str) -> IResult<&str, JsonNull> {
+    map(tag("null"), |_| JsonNull {})(input)
+}
+
+#[test]
+fn test_bool() {
+    assert_eq!(json_bool("false"), Ok(("", JsonBool::False)));
+    assert_eq!(json_bool("true"), Ok(("", JsonBool::True)));
+    assert!(json_bool("foo").is_err());
+}
+
+#[test]
+fn test_null() {
+    assert_eq!(json_null("null"), Ok(("", JsonNull {})));
+}
