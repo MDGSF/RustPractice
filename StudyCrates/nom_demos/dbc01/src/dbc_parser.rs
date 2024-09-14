8646c0d3c1e5cd4fff490b2d8ca200715412f341
huangjian <huangjian@bicv.com>
2024-09-14 11:54:33 +0800

    feat: update nom dbc

--- StudyCrates/nom_demos/dbc01/src/dbc_parser.rs
+++ StudyCrates/nom_demos/dbc01/src/dbc_parser.rs
@@ -82,6 +82,66 @@ pub struct DbcBusConfiguration(f64);
 #[derive(PartialEq, Debug, Clone)]
 pub struct DbcCanNodes(Vec<String>);
 
+/*
+ SG_ S7 m1 : 40|24@1- (1,0) [0|0] "" Vector__XXX
+ SG_ S8 m2 : 40|8@1- (1,0) [0|0] "" Vector__XXX
+ SG_ S6 M : 32|8@1- (1,0) [0|0] "" Vector__XXX
+ SG_ S3 m0 : 16|16@1- (1,0) [0|0] "" Vector__XXX
+ SG_ S2 m0 : 8|8@1- (1,0) [0|0] "" Vector__XXX
+*/
+#[derive(PartialEq, Debug, Clone)]
+pub enum DbcSignalMultiplexer {
+    M,
+    MultiplexerIdentifier(i64),
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub enum DbcSignalEndianness {
+    LittleEndian,
+    BigEndian,
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub enum DbcSignalSigned {
+    Signed,
+    Unsigned,
+}
+
+/// Signal definition.
+/// Format: `SG_ <SignalName> [M|m<MultiplexerIdentifier>] : <StartBit>|<Length>@<Endianness><Signed> (<Factor>,<Offset>) [<Min>|<Max>] "[Unit]" [ReceivingNodes]`
+/// Length in bits.
+/// Signed: + = unsigned; - = signed
+/// Endianness: 1 = little-endian, Intel; 0 = big-endian, Motorola
+/// M: If M than this signals contains a multiplexer identifier.
+/// MultiplexerIdentifier: Signal definition is only used if the value of the multiplexer signal equals to this value.
+#[derive(PartialEq, Debug, Clone)]
+pub struct DbcSignal {
+    pub name: String,
+    pub multiplexer: Option<DbcSignalMultiplexer>,
+    pub start_bit: i64,
+    pub length: i64,
+    pub endianness: DbcSignalEndianness,
+    pub signed: DbcSignalSigned,
+    pub factor: f64,
+    pub offset: f64,
+    pub min: Option<f64>,
+    pub max: Option<f64>,
+    pub unit: Option<String>,
+    pub receiving_node: Option<String>,
+}
+
+/// Message definition.
+/// Format: `BO_ <CAN-ID> <MessageName>: <MessageLength> <SendingNode>`
+/// MessageLength in bytes.
+#[derive(PartialEq, Debug, Clone)]
+pub struct DbcMessage {
+    pub can_id: i64,
+    pub name: String,
+    pub length: i64,
+    pub sending_node: String,
+    pub signals: Vec<DbcSignal>,
+}
+
 #[derive(PartialEq, Debug, Clone)]
 pub struct OneDbc {
     pub version: DbcVersion,
